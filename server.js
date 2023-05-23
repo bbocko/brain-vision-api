6c60e804112939e64c11c7c8be70c312c00d5cf8
Blaž <blaz.bocko1@gmail.com>
2023-05-23 20:12:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const database = {
     users: [
@@ -13,7 +14,7 @@ const database = {
             joined: new Date()
         },
         {
-            id: "111",
+            id: "124",
             username: "sally",
             email: "sally@gmail.com",
             password: "bananas",
@@ -24,11 +25,43 @@ const database = {
 }
 
 app.get("/", (req, res) => {
-    res.send("This is working");
+    res.send(database.users);
 })
 
 app.post("/signin", (req, res) => {
-    res.json("Signed in!");
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json("Successfully signed in");
+    } else {
+        res.status(400).json("Error logging in");
+    }
+})
+
+app.post("/register", (req, res) => {
+    const { username, email, password } = req.body;
+    database.users.push({
+        id: "125",
+        username: username,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.get("/profile/:id", (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).send("Not found");
+    }
 })
 
 app.listen(3000)
